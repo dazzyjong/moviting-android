9dad11688bf888325dc2aba88d6294f810f3200b
Jongseong Lee <dazzyjong@gmail.com>
2016-11-22 16:48:56 +0900

    server maintenance and version check dialog

--- app/src/main/java/com/moviting/android/ui/activity/SplashActivity.java
+++ app/src/main/java/com/moviting/android/ui/activity/SplashActivity.java
@@ -1,6 +1,10 @@
 package com.moviting.android.ui.activity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.crashlytics.android.Crashlytics;
@@ -10,17 +14,24 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+import com.moviting.android.BuildConfig;
 import com.moviting.android.R;
 import com.moviting.android.model.User;
 
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
 import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends BaseActivity {
 
     private static final String TAG = "SplashActivity";
+    private static final int SERVER_MAINTENANCE = 0;
+    private static final int OUT_VERSION = 1;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private boolean mIsAuthed;
     private SharedPreferences prefs = null;
@@ -39,10 +50,37 @@ public class SplashActivity extends BaseActivity {
         isFirstRun = prefs.getBoolean("firstrun", true);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
-        if (getFirebaseAuth().getCurrentUser() == null) {
-            Handler hd = new Handler();
-            hd.postDelayed(new splashhandler(), 1000);
-        }
+
+        getFirebaseDatabaseReference().child("server_status").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<String, Object> object = (HashMap)dataSnapshot.getValue();
+                Boolean maintenance = (Boolean)object.get("maintenance");
+                Long minVersion = (Long) object.get("min_version");
+                Long time = (Long) object.get("time");
+                if(maintenance) {
+                    createWarningDialog("서버가 점검중입니다.\n" + time + "시간 이후에 다시 접속해주세요.", SERVER_MAINTENANCE);
+                } else if( BuildConfig.VERSION_CODE < minVersion) {
+                    createWarningDialog("업데이트가 있습니다.\n앱을 업데이트 해주세요.", OUT_VERSION);
+                }else {
+                    if (getFirebaseAuth().getCurrentUser() == null) {
+                        Handler hd = new Handler();
+                        hd.postDelayed(new splashhandler(), 300);
+                    }
+                    getFirebaseAuth().addAuthStateListener(mAuthListener);
+                    mIsAuthed = false;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                if(getBaseContext() != null) {
+                    Toast.makeText(getBaseContext(), databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                Log.w(TAG, databaseError.getDetails());
+            }
+        });
+
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -57,7 +95,6 @@ public class SplashActivity extends BaseActivity {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
                             User user = dataSnapshot.getValue(User.class);
-                            try{ Thread.sleep(500);}catch(Exception ignored){}
 
                             if(user == null) {
                                 startActivity(LoginActivity.createIntent(SplashActivity.this));
@@ -87,7 +124,38 @@ public class SplashActivity extends BaseActivity {
                 }
             }
         };
+    }
 
+    private void createWarningDialog(String message, final int mode) {
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(SplashActivity.this);
+
+        // AlertDialog 셋팅
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("확인",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                if(mode == SERVER_MAINTENANCE) {
+                                    finish();
+                                } else if(mode == OUT_VERSION) {
+                                    final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+                                    try {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                                        finish();
+                                    } catch (android.content.ActivityNotFoundException anfe) {
+                                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                                        finish();
+                                    }
+                                }
+                            }
+                        });
+
+        // 다이얼로그 생성
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // 다이얼로그 보여주기
+        alertDialog.show();
     }
 
     private class splashhandler implements Runnable{
@@ -115,8 +183,7 @@ public class SplashActivity extends BaseActivity {
     @Override
     public void onStart() {
         super.onStart();
-        getFirebaseAuth().addAuthStateListener(mAuthListener);
-        mIsAuthed = false;
+
     }
 
     @Override
